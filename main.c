8334ae8d9f3a26f9aaae2af2ba8dba2105da7dbc
iot-lsy <suwonbank@gmail.com>
2021-04-05 22:44:27 +0900

    ~거듭제곱

--- main.c
+++ main.c
@@ -1,18 +1,61 @@
 #include <stdio.h>
 
-int factorial(int n){
+int factorial_recursion(int n){ //재귀문 팩토리
 
-    printf("factorial(%d)\n",n);
-    if(n<=1) return 1;
-    else return (n * factorial(n-1));
+    if(n>=1) return (n * factorial_recursion(n-1));
+    else return 1;
+
+}
+
+int factorial_iter(int n){ // 반복문 팩토리얼
+
+    int res = 1;
+
+    for(int i=0;i<n;i++){
+        res *= (i+1);
+    }
+
+    return res;
+}
+
+double slow_power(double x, int n){ // 거듭제곱 반복문
+
+    double res = 1.0;
+
+    for(int i=0; i<n; i++){
+        res *= x;
+    }
+
+    return res;
+}
+
+double power(double x, int n){// 거듭제곱 순환문
+
+    if(n==0) return 1;
+    else if(n % 2 == 0){
+        return power(x*x, n/2);
+    }else{
+        return x * power(x*x, (n-1)/2);
+    }
 
 }
 
 int main() {
 
-    int res = 0;
+    int res1 = 0, res2 = 0;
+    double res3 = 0;
+    double res4 = 0;
 
-    res = factorial(5);
+    res1 = factorial_recursion(5);
 
-    printf("%d",res);
+    res2 = factorial_iter(5);
+
+    res3 = slow_power(1.4, 3);
+
+    res4 = power(1.4, 3);
+
+    printf("factorial_recursion : %d\n",res1);
+    printf("factorial_iter : %d\n", res2);
+    printf("slow_power : %lf\n", res3);
+    printf("power : %lf\n", res4);
 }
